d0a65f44288c93bb63e1a6e74502129b8d15f074
fazle <ryan7998@gmail.com>
2021-01-07 21:24:30 -0500

    comment added

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+/* Declare global variables: */
 var body = document.body;
 var corrAnsSum = 0;
 var flag = 0;
@@ -7,7 +8,9 @@ var highScorearr = [];
 var finalScore = 0;
 var timeInterval = 0;
 
-// Question Bank:
+/**
+ * Create quizQuestions array with objects values of question, answer and correct answers
+ */
 var quizQuestions = [
 	{
 		question: "Is JavaScript a case-sensitive language?",
@@ -59,11 +62,11 @@ var quizQuestions = [
 	}
 ];
 
+// Create Start Button for Home Page:
 var startBtnEl = document.createElement("button");
 startBtnEl.innerText="Start Test";
 
-// Create first page Elements:
-
+// Create Home page Elements:
 var h1El = document.createElement('h1');
 h1El.textContent = 'Welcome to my page';
 
@@ -76,6 +79,7 @@ body.appendChild(fpageDiv);
 var headerDiv = document.createElement('div');
 var hscoreDiv = document.createElement('div');
 
+// Set High Score link: On Click initiate getHighScore function
 hscoreDiv.innerHTML = '<a onclick="getHighScore()" href="javascript:;"> Show High Score </a>';
 hscoreDiv.querySelector('a').setAttribute('style', 'text-decoration:none');
 
@@ -123,48 +127,65 @@ btnSaveScore.setAttribute('style', 'background: #171f4f; color: #fff; border-rad
 // button to go back:
 var btnGoback = document.createElement('button');
 btnGoback.innerText="Go back";
-btnGoback.setAttribute('style', 'background: #171f4f; color: #fff; border-radius: 5px; padding:5px; margin: 0 10px');
+btnGoback.setAttribute('style', 'background: #171f4f;color: #fff;border-radius: 5px;padding: 1em;font-size: 1em');
 
 // button clear high score:
 var btnClearScore = document.createElement('button');
 btnClearScore.innerText="Clear Score";
-btnClearScore.setAttribute('style', 'background: #171f4f; color: #fff; border-radius: 5px; padding:5px; margin: 0 10px');
+btnClearScore.setAttribute('style', 'background: #171f4f;color: #fff;border-radius: 5px;padding: 1em;font-size: 1em');
 
 
-// set css style:
+// set css style for home page:
 body.setAttribute('style', 'font-family: sans-serif; font-family: sans-serif; padding: 20px; margin: 20px;');
 headerDiv.setAttribute('style', 'display: flex; justify-content: space-between; margin:20px;');
 fpageDiv.setAttribute('style', 'margin-top: 5em; display: flex; flex-direction: column; align-items: center; justify-content: center;');
 startBtnEl.setAttribute('style', 'width: 20%; height: 100px; background: #171f4f; color: #fff; font-size: 2em; border-radius: 10px;');
 
-// Start Timer Function:
+/*  Start Timer Function:
+    Set Time to 60 seconds. If Time is over clear Interval and initiate endQuiz function with parameter string type: 'timeup'
+    For every wrong answer deduct 10 seconds from the timer 
+*/
 function startTimer(){
-    timeLeft = 2000;
+    timeLeft = 60;
     timeInterval = setInterval(function() {
         timerDiv.textContent = 'Time Left: ' + timeLeft + ' s';
         timeLeft--;
-        if(timeLeft == 0){
+        if(timeLeft <= 0){
             clearInterval(timeInterval);
-            endQuiz();
+            endQuiz('timeup');
         }
     }, 1000);
 }
 
-// function end quiz:
-function endQuiz(){
-    finalScore = timeLeft;
-    clearInterval(timeInterval);
-    timerDiv.textContent="";
-    mainDiv.innerHTML="<h2>All Done!!</h2> Your final score is: " + finalScore + ".</br></br>";
-    
-    // Show input and button to sace high score
-    mainDiv.innerHTML += "<label>Enter Initials: </label>";
-    mainDiv.appendChild(inputSaveScore);
-    mainDiv.appendChild(btnSaveScore);
+/*  End Quiz Function:
+    This is initiated after time is over from startTimer function
+    Gets parameter el: if value is timeup, set message to Time Up and set score to  0.
+    If initiated after the Quiz is over Show Final score  And asks to save score with Initials
+*/ 
+function endQuiz(el){
+    if(el == 'timeup'){
+        finalScore = 0;
+        clearInterval(timeInterval);
+        timerDiv.textContent="";
+        mainDiv.innerHTML="<h2>Time Up!!</h2> Your final score is: " + finalScore + ".</br></br>";
+    }else{
+        finalScore = timeLeft;
+        clearInterval(timeInterval);
+        timerDiv.textContent="";
+        mainDiv.innerHTML="<h2>All Done!!</h2> Your final score is: " + finalScore + ".</br></br>";
+        
+        // Show input and button to sace high score
+        mainDiv.innerHTML += "<label>Enter Initials: </label>";
+        mainDiv.appendChild(inputSaveScore);
+        mainDiv.appendChild(btnSaveScore);
+    }   
 }
 
 
-// function show questions
+/* Show Question Function
+ * Called by Start Quiz, Parameter: Obj of Question 
+ * Call Print Question to show Question in HTML
+*/
 function showQuestion(question){
     var ques = question.question;
     var ans = question.answers;
@@ -174,7 +195,12 @@ function showQuestion(question){
     printQuestion(ques, ans, corAns);
 }
 
-// function print questions
+/**
+ * Print Question Function
+ * Called by showQuestion
+ * Set Html Element and css programically ans print question
+ * Set correct answer attribute on the link
+ */
 function printQuestion(ques, ans, corAns){
     qDiv.textContent = "";
     aDiv.textContent="";
@@ -190,6 +216,12 @@ function printQuestion(ques, ans, corAns){
     aDiv.setAttribute('style', 'display:flex; flex-direction:column');
 }
 
+/**
+ * Check Answer function:
+ * get correct answer value from attribute and check if correct answer is clicked
+ * if correct answer set text to Correct Answer 
+ * if wrong then deduct 10s from timer and set text to wrong Answer.
+ */
 function check(event) {
     var getAns = event.target.getAttribute("value");
     var correctAns = event.target.getAttribute("correctans");
@@ -207,7 +239,10 @@ function check(event) {
 }
 
 
-// Start Quiz Function:
+/**
+ * Start Quiz function:
+ * initiate show question function if there are more question in the question bank
+ */
 function startQuiz(){
     if(flag < quizQuestions.length ){
         showQuestion(quizQuestions[flag]);
@@ -216,6 +251,12 @@ function startQuiz(){
     }
 }
 
+/**
+ * get High Score function:
+ * clear all divs
+ * get data from localstorage and show in html
+ * if localstorage empty show text that no high score set yet.
+ */
 function getHighScore(){
     mainDiv.textContent = '';
     fpageDiv.textContent='';
@@ -224,32 +265,64 @@ function getHighScore(){
 
     var highScorearr = JSON.parse(localStorage.getItem("highscoredetails"));
 
+    headerDiv.innerHTML="<h1> High Score Page </h1>";
+    headerDiv.setAttribute('style', 'display: flex;justify-content: center;margin:20px;');
+
     if(highScorearr != null){
+        highScoreDiv = document.createElement('div');
+        highScoreDiv.setAttribute('style', 'display: flex; align-items: center; justify-content: space-around; background: #faebd7; padding: .5em; margin:1px;');
+        nameDiv = document.createElement('h3');
+        scoreDiv = document.createElement('h3');
+
+        nameDiv.textContent = 'Name';
+        scoreDiv.textContent = 'Score'
+        highScoreDiv.appendChild(nameDiv);
+        highScoreDiv.appendChild(scoreDiv);
+        mainDiv.append(highScoreDiv);
+        mainDiv.innerHTML += "";
+
         for(i = 0; i<highScorearr.length; i++){
-            mainDiv.innerHTML += 'Name: '+ highScorearr[i].initials + '. High Score: ' + highScorearr[i].highscore + "</br>";
+            nameDiv.textContent = highScorearr[i].initials;
+            scoreDiv.textContent = highScorearr[i].highscore;
+            highScoreDiv.appendChild(nameDiv);
+            highScoreDiv.appendChild(scoreDiv);
+            mainDiv.append(highScoreDiv);
+            mainDiv.innerHTML += "";
         }
     }else{
         mainDiv.textContent = 'No High score set yet';
     }
-    mainDiv.appendChild(btnGoback);
-    mainDiv.appendChild(btnClearScore);
+    btnDiv = document.createElement('div');
+    btnDiv.setAttribute('style', 'display:flex; justify-content:space-around; margin: 2em;');
+
+    btnDiv.appendChild(btnGoback);
+    btnDiv. appendChild(btnClearScore);
+
+    mainDiv.appendChild(btnDiv);
 }
 
-// Start button Function:
+/**
+ * Start button handler
+ * remove homepage content
+ * start timer and quiz function
+ */
 var startBtnHandler = function(event){
     fpageDiv.remove();
     startTimer();
     startQuiz();
 }
 
-// Save high score Handler:
+/**
+ * Save high score handler
+ * Get data from textbox and store it in localstorage
+ * if textbox empty show alert to enter initials
+ * Sort by high score ans show high score.
+ */
 var saveHighScoreHandler = function(event){
     var getInitials = document.getElementById('hscoreEl').value;
-    //console.log(getInitials);
     if(getInitials == ""){
         alert('Please Enter your initials ');
     }else{
-        // create obj for high score:
         var highScoreObj = {
             initials: getInitials,
             highscore: finalScore
@@ -269,10 +342,12 @@ var saveHighScoreHandler = function(event){
     }
 }
 
-// Back Button Handler:
+// Back Button Handler: Go back to Home Page
 var backButtonHandler = function(event){
     location.reload();
 }
+
+// Clear Score Button Handler: Delete local storage data and go back to home page
 var clearScoreHandler = function(event){
     localStorage.clear();
     location.reload();
@@ -287,4 +362,5 @@ btnSaveScore.addEventListener("click", saveHighScoreHandler);
 // Go Back button Click:
 btnGoback.addEventListener("click", backButtonHandler);
 
+// Clear Score button Click:
 btnClearScore.addEventListener("click", clearScoreHandler);
